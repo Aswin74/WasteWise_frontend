a98d429c0f4680ee8c680a9501b67644d0bf7ad2
Aswin <jayaprakash.jp713@gmail.com>
2024-04-20 15:29:48 +0530

    NativeWind, ReAnimated installation

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar"
+import Animated, { BounceIn } from "react-native-reanimated"
+import { StyleSheet, Text, View } from "react-native"
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Intial Commit</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text className="text-red-700">Initial Commit</Text>
+            <Text className="text-blue-700">Integrated TailWind CSS</Text>
+            <Animated.Text entering={BounceIn} className="text-green-700">
+                Integrated React ReAnimated
+            </Animated.Text>
+            <StatusBar style="auto" />
+        </View>
+    )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+})
