472307509c37816546fc1eac1a4b604d83cf12ec
Aswin <jayaprakash.jp713@gmail.com>
2024-04-21 23:19:08 +0530

    Animated Login Components

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -1,6 +1,12 @@
 import { StatusBar } from "expo-status-bar"
 import React from "react"
 import { View, Text, Image, TextInput, TouchableOpacity } from "react-native"
+import Animated, {
+    FadeIn,
+    FadeInDown,
+    FadeInUp,
+    FadeOut,
+} from "react-native-reanimated"
 
 function Login() {
     return (
@@ -13,57 +19,85 @@ function Login() {
 
             {/* Logo and extra icons */}
             <View className="top-28 w-full items-center">
-                <Image
+                <Animated.Image
+                    entering={FadeInUp.delay(200)
+                        .duration(1000)
+                        .springify()
+                        .damping(8)}
                     className="h-20 w-20 rounded-full"
                     source={require("../../assets/WasteWise-Logo.jpg")}
                 />
-                <Text
+                <Animated.Text
+                    entering={FadeInUp.delay(400).duration(1000).springify()}
                     style={{ fontFamily: "Inter-Regular" }}
                     className="p-2 font-bold text-white text-4xl tracking-wider"
                 >
                     WasteWise
-                </Text>
+                </Animated.Text>
 
-                <View className="flex items-center mt-3">
+                <Animated.View
+                    entering={FadeInUp.delay(600).duration(1000).springify()}
+                    className="flex items-center mt-3"
+                >
                     <Text className="text-white font-bold text-3xl tracking-wider">
                         Login
                     </Text>
-                </View>
+                </Animated.View>
             </View>
 
             {/* login form */}
             <View className="h-full w-full flex pt-44">
                 {/* form */}
                 <View className="mx-4 space-y-4">
-                    <View className="bg-black/5 rounded-2xl p-4">
+                    <Animated.View
+                        entering={FadeInDown.delay(200)
+                            .duration(1000)
+                            .springify()}
+                        className="bg-black/5 rounded-2xl p-4"
+                    >
                         <TextInput
                             placeholder="Username"
                             placeholderTextColor={"gray"}
                         />
-                    </View>
+                    </Animated.View>
 
-                    <View className="bg-black/5 rounded-2xl p-4 mb-2">
+                    <Animated.View
+                        entering={FadeInDown.delay(300)
+                            .duration(1000)
+                            .springify()}
+                        className="bg-black/5 rounded-2xl p-4 mb-2"
+                    >
                         <TextInput
                             placeholder="Password"
                             placeholderTextColor={"gray"}
                             secureTextEntry
                         />
-                    </View>
+                    </Animated.View>
 
-                    <View className="w-full">
+                    <Animated.View
+                        entering={FadeInDown.delay(400)
+                            .duration(1000)
+                            .springify()}
+                        className="w-full"
+                    >
                         <TouchableOpacity className="bg-sky-500 w-full rounded-2xl p-3">
                             <Text className="text-white text-xl font-semibold text-center">
                                 Login
                             </Text>
                         </TouchableOpacity>
-                    </View>
+                    </Animated.View>
 
-                    <View className="flex-row justify-center">
+                    <Animated.View
+                        entering={FadeInDown.delay(500)
+                            .duration(1000)
+                            .springify()}
+                        className="flex-row justify-center"
+                    >
                         <Text>Don't have an account? </Text>
                         <TouchableOpacity>
                             <Text className="text-sky-600">SignUp</Text>
                         </TouchableOpacity>
-                    </View>
+                    </Animated.View>
                 </View>
             </View>
         </View>
