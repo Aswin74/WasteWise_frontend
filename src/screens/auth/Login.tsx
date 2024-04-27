f869920f29569d7458cbf3beacd3f69dd453b409
Aswin <jayaprakash.jp713@gmail.com>
2024-04-27 21:41:34 +0530

    Input Keyboard View Correction

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -1,6 +1,15 @@
 import { StatusBar } from "expo-status-bar"
-import React from "react"
-import { View, Text, Image, TextInput, TouchableOpacity } from "react-native"
+import React, { useState } from "react"
+import {
+    View,
+    Text,
+    Image,
+    TextInput,
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView,
+} from "react-native"
 import Animated, {
     FadeIn,
     FadeInDown,
@@ -11,104 +20,129 @@ import Animated, {
 // Navigation
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { RootStackParamList } from "../../navigation/StackNavigator"
+//type
+import { LoginInput } from "../../types"
 
 type LoginProps = NativeStackScreenProps<RootStackParamList, "Login">
 
 const Login = ({ navigation }: LoginProps) => {
+    const [inputValues, setInputValues] = useState<LoginInput>({
+        username: "",
+        password: "",
+    })
+
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { id, value } = e.target
+        setInputValues((prevState) => ({ ...prevState, [id]: value }))
+        console.log(inputValues)
+    }
+
     return (
-        <View className="w-full h-full bg-white">
-            <StatusBar style="inverted" />
-            <Image
-                className="h-full w-full absolute bottom-10"
-                source={require("../../assets/wave.png")}
-            />
-
-            {/* Logo and extra icons */}
-            <View className="top-28 w-full items-center">
-                <Animated.Image
-                    entering={FadeInUp.delay(200)
-                        .duration(1000)
-                        .springify()
-                        .damping(8)}
-                    className="h-20 w-20 rounded-full"
-                    source={require("../../assets/WasteWise-Logo.jpg")}
+        <KeyboardAvoidingView
+            behavior="padding"
+            keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 10}
+            className="flex-1 bg-white"
+        >
+            <ScrollView>
+                <StatusBar style="inverted" />
+                {/* Background Image */}
+                <Image
+                    className="h-full w-full absolute"
+                    source={require("../../assets/wave.png")}
                 />
-                <Animated.Text
-                    entering={FadeInUp.delay(400).duration(1000).springify()}
-                    style={{ fontFamily: "Inter-Regular" }}
-                    className="p-2 font-bold text-white text-4xl tracking-wider"
-                >
-                    WasteWise
-                </Animated.Text>
-
-                <Animated.View
-                    entering={FadeInUp.delay(600).duration(1000).springify()}
-                    className="flex items-center mt-3"
-                >
-                    <Text className="text-white font-bold text-3xl tracking-wider">
-                        Login
-                    </Text>
-                </Animated.View>
-            </View>
-
-            {/* login form */}
-            <View className="h-full w-full flex pt-44">
-                {/* form */}
-                <View className="mx-4 space-y-4">
-                    <Animated.View
-                        entering={FadeInDown.delay(200)
+                {/* Logo and extra icons */}
+                <View className="top-28 w-full items-center pb-6">
+                    <Animated.Image
+                        entering={FadeInUp.delay(200)
+                            .duration(1000)
+                            .springify()
+                            .damping(8)}
+                        className="h-20 w-20 rounded-full"
+                        source={require("../../assets/WasteWise-Logo.jpg")}
+                    />
+                    <Animated.Text
+                        entering={FadeInUp.delay(400)
                             .duration(1000)
                             .springify()}
-                        className="bg-black/5 rounded-2xl p-4"
+                        style={{ fontFamily: "Inter-Regular" }}
+                        className="p-2 mt-3 font-bold text-white text-4xl tracking-wider"
                     >
-                        <TextInput
-                            placeholder="Username"
-                            placeholderTextColor={"gray"}
-                        />
-                    </Animated.View>
-
-                    <Animated.View
-                        entering={FadeInDown.delay(300)
-                            .duration(1000)
-                            .springify()}
-                        className="bg-black/5 rounded-2xl p-4 mb-2"
-                    >
-                        <TextInput
-                            placeholder="Password"
-                            placeholderTextColor={"gray"}
-                            secureTextEntry
-                        />
-                    </Animated.View>
-
-                    <Animated.View
-                        entering={FadeInDown.delay(400)
-                            .duration(1000)
-                            .springify()}
-                        className="w-full"
-                    >
-                        <TouchableOpacity className="bg-sky-500 w-full rounded-2xl p-3">
-                            <Text className="text-white text-xl font-semibold text-center">
-                                Login
-                            </Text>
-                        </TouchableOpacity>
-                    </Animated.View>
-
-                    <Animated.View
-                        entering={FadeInDown.delay(500)
-                            .duration(1000)
-                            .springify()}
-                        className="flex-row justify-center"
-                    >
-                        <Text>Don't have an account? </Text>
-                        <TouchableOpacity
-                            onPress={() => navigation.push("SignUp")}
-                        >
-                            <Text className="text-sky-600">SignUp</Text>
-                        </TouchableOpacity>
-                    </Animated.View>
+                        WasteWise
+                    </Animated.Text>
                 </View>
-            </View>
-        </View>
+
+                {/* login form */}
+                <View className="h-full w-full flex pt-24">
+                    {/* form */}
+                    <Animated.View
+                        entering={FadeInUp.delay(600)
+                            .duration(1000)
+                            .springify()}
+                        className="flex items-center mt-10 mb-5"
+                    >
+                        <Text className="text-sky-500 font-bold text-3xl tracking-wider">
+                            Login
+                        </Text>
+                    </Animated.View>
+                    <View className="mx-4 space-y-4">
+                        <Animated.View
+                            entering={FadeInDown.delay(200)
+                                .duration(1000)
+                                .springify()}
+                            className="bg-black/5 rounded-2xl p-4"
+                        >
+                            <TextInput
+                                // id="username"
+                                // value={inputValues.username}
+                                placeholder="Username"
+                                placeholderTextColor={"gray"}
+                                // onChangeText={handleInputChange}
+                            />
+                        </Animated.View>
+
+                        <Animated.View
+                            entering={FadeInDown.delay(300)
+                                .duration(1000)
+                                .springify()}
+                            className="bg-black/5 rounded-2xl p-4 mb-2"
+                        >
+                            <TextInput
+                                placeholder="Password"
+                                placeholderTextColor={"gray"}
+                                secureTextEntry
+                            />
+                        </Animated.View>
+
+                        <Animated.View
+                            entering={FadeInDown.delay(400)
+                                .duration(1000)
+                                .springify()}
+                            className="w-full"
+                        >
+                            <TouchableOpacity className="bg-sky-500 w-full rounded-2xl p-3">
+                                <Text className="text-white text-xl font-semibold text-center">
+                                    Login
+                                </Text>
+                            </TouchableOpacity>
+                        </Animated.View>
+
+                        <Animated.View
+                            entering={FadeInDown.delay(500)
+                                .duration(1000)
+                                .springify()}
+                            className="flex-row justify-center"
+                        >
+                            <Text>Don't have an account? </Text>
+                            <TouchableOpacity
+                                onPress={() => navigation.push("SignUp")}
+                            >
+                                <Text className="text-sky-600">SignUp</Text>
+                            </TouchableOpacity>
+                        </Animated.View>
+                    </View>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
 
--- src/screens/auth/SignUp.tsx
+++ src/screens/auth/SignUp.tsx
@@ -1,6 +1,15 @@
 import { StatusBar } from "expo-status-bar"
 import React from "react"
-import { View, Text, Image, TextInput, TouchableOpacity } from "react-native"
+import {
+    View,
+    Text,
+    Image,
+    TextInput,
+    TouchableOpacity,
+    KeyboardAvoidingView,
+    Platform,
+    ScrollView,
+} from "react-native"
 import Animated, {
     FadeIn,
     FadeInDown,
@@ -16,111 +25,120 @@ type SignUpProps = NativeStackScreenProps<RootStackParamList, "SignUp">
 
 const SignUp = ({ navigation }: SignUpProps) => {
     return (
-        <View className="w-full h-full bg-white">
+        <KeyboardAvoidingView
+            behavior="padding"
+            keyboardVerticalOffset={Platform.OS === "ios" ? 100 : 10}
+            className="w-full h-full bg-white"
+        >
             <StatusBar style="inverted" />
-            <Image
-                className="h-full w-full absolute bottom-10"
-                source={require("../../assets/wave.png")}
-            />
-
-            {/* Logo and extra icons */}
-            <View className="top-28 w-full items-center">
-                <Animated.Image
-                    entering={FadeInUp.delay(200)
-                        .duration(1000)
-                        .springify()
-                        .damping(8)}
-                    className="h-20 w-20 rounded-full"
-                    source={require("../../assets/WasteWise-Logo.jpg")}
+            <ScrollView>
+                <Image
+                    className="h-full w-full absolute bottom-10"
+                    source={require("../../assets/wave.png")}
                 />
-                <Animated.Text
-                    entering={FadeInUp.delay(400).duration(1000).springify()}
-                    style={{ fontFamily: "Inter-Regular" }}
-                    className="p-2 font-bold text-white text-4xl tracking-wider"
-                >
-                    WasteWise
-                </Animated.Text>
 
-                <Animated.View
-                    entering={FadeInUp.delay(600).duration(1000).springify()}
-                    className="flex items-center mt-3"
-                >
-                    <Text className="text-white font-bold text-3xl tracking-wider">
-                        SignUp
-                    </Text>
-                </Animated.View>
-            </View>
-
-            {/* SignUp form */}
-            <View className="h-full w-full flex pt-44">
-                {/* form */}
-                <View className="mx-4 space-y-4">
-                    <Animated.View
-                        entering={FadeInDown.delay(200)
+                {/* Logo and extra icons */}
+                <View className="top-28 w-full items-center">
+                    <Animated.Image
+                        entering={FadeInUp.delay(200)
+                            .duration(1000)
+                            .springify()
+                            .damping(8)}
+                        className="h-20 w-20 rounded-full"
+                        source={require("../../assets/WasteWise-Logo.jpg")}
+                    />
+                    <Animated.Text
+                        entering={FadeInUp.delay(400)
                             .duration(1000)
                             .springify()}
-                        className="bg-black/5 rounded-2xl p-4"
+                        style={{ fontFamily: "Inter-Regular" }}
+                        className="p-2 mt-3 font-bold text-white text-4xl tracking-wider"
                     >
-                        <TextInput
-                            placeholder="Username"
-                            placeholderTextColor={"gray"}
-                        />
-                    </Animated.View>
-
-                    <Animated.View
-                        entering={FadeInDown.delay(300)
-                            .duration(1000)
-                            .springify()}
-                        className="bg-black/5 rounded-2xl p-4"
-                    >
-                        <TextInput
-                            placeholder="Email"
-                            placeholderTextColor={"gray"}
-                        />
-                    </Animated.View>
-
-                    <Animated.View
-                        entering={FadeInDown.delay(400)
-                            .duration(1000)
-                            .springify()}
-                        className="bg-black/5 rounded-2xl p-4 mb-2"
-                    >
-                        <TextInput
-                            placeholder="Password"
-                            placeholderTextColor={"gray"}
-                            secureTextEntry
-                        />
-                    </Animated.View>
-
-                    <Animated.View
-                        entering={FadeInDown.delay(500)
-                            .duration(1000)
-                            .springify()}
-                        className="w-full"
-                    >
-                        <TouchableOpacity className="bg-sky-500 w-full rounded-2xl p-3">
-                            <Text className="text-white text-xl font-semibold text-center">
-                                SignUp
-                            </Text>
-                        </TouchableOpacity>
-                    </Animated.View>
-
-                    <Animated.View
-                        entering={FadeInDown.delay(600)
-                            .duration(1000)
-                            .springify()}
-                        className="flex-row justify-center"
-                    >
-                        <Text>Already have an account? </Text>
-                        <TouchableOpacity
-                            onPress={() => navigation.push("Login")}
-                        >
-                            <Text className="text-sky-600">Login</Text>
-                        </TouchableOpacity>
-                    </Animated.View>
+                        WasteWise
+                    </Animated.Text>
                 </View>
-            </View>
-        </View>
+
+                {/* SignUp form */}
+                <View className="h-full w-full flex pt-40">
+                    {/* form */}
+                    <Animated.View
+                        entering={FadeInUp.delay(600)
+                            .duration(1000)
+                            .springify()}
+                        className="flex items-center mb-5"
+                    >
+                        <Text className="text-sky-500 font-bold text-3xl tracking-wider">
+                            SignUp
+                        </Text>
+                    </Animated.View>
+                    <View className="mx-4 space-y-4">
+                        <Animated.View
+                            entering={FadeInDown.delay(200)
+                                .duration(1000)
+                                .springify()}
+                            className="bg-black/5 rounded-2xl p-4"
+                        >
+                            <TextInput
+                                placeholder="Username"
+                                placeholderTextColor={"gray"}
+                            />
+                        </Animated.View>
+
+                        <Animated.View
+                            entering={FadeInDown.delay(300)
+                                .duration(1000)
+                                .springify()}
+                            className="bg-black/5 rounded-2xl p-4"
+                        >
+                            <TextInput
+                                placeholder="Email"
+                                placeholderTextColor={"gray"}
+                            />
+                        </Animated.View>
+
+                        <Animated.View
+                            entering={FadeInDown.delay(400)
+                                .duration(1000)
+                                .springify()}
+                            className="bg-black/5 rounded-2xl p-4 mb-2"
+                        >
+                            <TextInput
+                                placeholder="Password"
+                                placeholderTextColor={"gray"}
+                                secureTextEntry
+                            />
+                        </Animated.View>
+
+                        <Animated.View
+                            entering={FadeInDown.delay(500)
+                                .duration(1000)
+                                .springify()}
+                            className="w-full"
+                        >
+                            <TouchableOpacity className="bg-sky-500 w-full rounded-2xl p-3">
+                                <Text className="text-white text-xl font-semibold text-center">
+                                    SignUp
+                                </Text>
+                            </TouchableOpacity>
+                        </Animated.View>
+
+                        <Animated.View
+                            entering={FadeInDown.delay(600)
+                                .duration(1000)
+                                .springify()}
+                            className="flex-row justify-center"
+                        >
+                            <Text>Already have an account? </Text>
+                            <TouchableOpacity
+                                onPress={() => navigation.push("Login")}
+                            >
+                                <Text className="text-sky-600">Login</Text>
+                            </TouchableOpacity>
+                        </Animated.View>
+                    </View>
+                </View>
+            </ScrollView>
+        </KeyboardAvoidingView>
     )
 }
 
