83f7eef61dcef4b512bdf5a3b05bb79b8c83cd99
KajolChand <kajolchand11@gmail.com>
2024-06-20 16:46:39 +0530

    input component

---
+++ src/components/InputBox.tsx
@@ -0,0 +1,20 @@
+import { Text, TextInput, View } from "react-native"
+
+interface InputProp{
+    placeholder:string,
+}
+
+const InputBox:React.FC<InputProp>= ({placeholder}) =>{
+    return(
+        <View
+        className="bg-ww-black/5 rounded-2xl p-4"
+      >
+        <TextInput
+          placeholder={placeholder}
+          placeholderTextColor={"gray"}
+        />
+      </View>
+    )
+}
+
+export default InputBox
--- src/screens/Contact/Contact.tsx
+++ src/screens/Contact/Contact.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native"
 import React from "react"
 
+
 const Contact = () => {
   return (
     <View>
       <Text>Contact</Text>
+    
     </View>
   )
 }
