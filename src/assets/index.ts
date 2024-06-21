9fc277a03525094ad8a6f364c63be5f1108e4606
KajolChand <kajolchand11@gmail.com>
2024-06-21 17:48:48 +0530

    vector icons installation

---
+++ src/assets/index.ts
@@ -0,0 +1,10 @@
+import ww from './logo/ww.png'
+import ww_sm from './ww_sm.png'
+import ww_str from './logo/ww_logo_str.png'
+import ww_str_sm from './logo/ww_logo_str_sm.png'
+import ww_logo from './logo/ww_logo.png'
+import ww_logo_sm from './logo/ww_logo_sm.png'
+import ww_text from './ww_text.png'
+import ww_text_sm from './ww_text_sm.png'
+
+export {ww,ww_sm,ww_str_sm,ww_str,ww_logo,ww_logo_sm,ww_text_sm,ww_text}
