995fc2f168a7f6d2f917112bd108c407aed1897f
KajolChand <kajolchand11@gmail.com>
2024-06-21 12:21:19 +0530

    logo

--- src/screens/Feedback/Feedback.tsx
+++ src/screens/Feedback/Feedback.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import InputBox from '../../components/InputBox';
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import InputBox from "../../components/InputBox";
 
 const Feedback: React.FC = () => {
-  return (
-    <View className='flex-1 p-5 bg-ww-white'>
-      <Text className='text-xl font-bold mb-5' >Feedback</Text>
-      <InputBox placeholder="Enter your name" />
-      <InputBox placeholder="Enter your email" />
-      <InputBox placeholder="Your feedback" />
+    return (
+        <View className="flex-1 p-5 bg-ww-white">
+            <Text className="text-xl font-bold mb-5">Feedback</Text>
+            <InputBox placeholder="Enter your name" />
+            <InputBox placeholder="Enter your email" />
+            <InputBox placeholder="Your feedback" />
 
-      <TouchableOpacity className='bg-ww-primary p-4 rounded-xl align-middle my-5'>
-        <Text className='text-ww-white text-xl font-bold text-center'>Submit</Text>
-      </TouchableOpacity>
-    </View>
-  );
+            <TouchableOpacity className="bg-ww-primary p-4 rounded-xl align-middle my-5">
+                <Text className="text-ww-white text-xl font-bold text-center">
+                    Submit
+                </Text>
+            </TouchableOpacity>
+        </View>
+    );
 };
-
 
 export default Feedback;
